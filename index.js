92b56ada11b4da5670fdaaa1de3a002b6b288868
asifiqbalsagor <asifiqbalsagor98@gmail.com>
2022-11-13 23:13:21 +0600

    index.js file added

--- index.js
+++ index.js
@@ -63,4 +63,48 @@ function run() {
       res.send(CreateService);
     });
 
-   
+    app.post("/CreateService", async (req, res) => {
+      const CreateService = req.body;
+      const result = await serviceCollection.insertOne(CreateService);
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
+
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.get("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { foodId: id };
+      const cursor = reviewCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run();
+
+app.listen(port, () => {
+  console.log(" server in running : ", { port });
+});
+module.exports = app; 
